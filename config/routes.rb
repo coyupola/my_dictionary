375df42c22158add572157488aec611d65ddbbc1
coyupola <yuka@kamiyamayuka-no-MacBook-Air.local>
2016-07-02 14:15:47 +0900

    質問をタイムラインに表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
-  resources :questions, only: [:create]
+  resource :users, only: [:show, :edit, :update]
+  resource :questions, only: [:create]
 root 'top#index'
 end
