9885693763869bfcfb39673520e2956f4deaad9e
coyupola <yuka@kamiyamayuka-no-MacBook-Air.local>
2016-07-05 07:55:40 +0900

    回答ページを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
   has_many :questions, ->{ order("created_at DESC")}
+  has_many :answers, ->{ order("updated_at DESC")}
 end
